96fa52bf9f6b5049b65a60bf05e51e4e7955e5a1
Kiptum <duncankipkoech12@gmail.com>
2025-08-02 08:58:45 +0300

    quiz

---
+++ quiz.js
@@ -0,0 +1,19 @@
+function checkAnswer (){
+    let correctAnswer  = "4";
+    let quiz = document.querySelector("#choice1");
+    
+    let userAnswer = quiz.value;
+
+    if(userAnswer === correctAnswer){
+        document.getElementById("feedback").textContent = "Correct! Well done."        
+    }
+    else{
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!"
+
+    }
+
+    let button = document.getElementById("submit-answer")
+    button.addEventListener("click", function(checkAnswer){
+
+    })
+}
