680922693a02c0419f4a42854f459e2d6caf8a6b
Kiptum <duncankipkoech12@gmail.com>
2025-08-02 19:32:52 +0300

    quiz

--- quiz.js
+++ quiz.js
@@ -1,17 +1,14 @@
 function checkAnswer (){
     let correctAnswer  = "4";
-    let userAnswer = document.querySelector("input[name= 'quiz]:checked").value;
-      
-
+    let userAnswer = document.querySelector("input[name= 'quiz]:checked").value;   
     if(userAnswer === correctAnswer){
-        document.getElementById("feedback").textContent = "Correct! Well done."        
-    }
+        document.getElementById("feedback").textContent = "Correct! Well done."
+    }  
+
     else{
         document.getElementById("feedback").textContent = "That's incorrect. Try again!"
 
     }
-
-    
 }
 
 let button = document.getElementById("submit-answer")
