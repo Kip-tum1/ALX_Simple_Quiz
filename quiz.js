47f111445cb79ab0bced96f0122c3179c0989a94
Kiptum <duncankipkoech12@gmail.com>
2025-08-02 19:56:54 +0300

    calculator

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 function checkAnswer (){
     let correctAnswer  = "4";
     let userAnswer = document.querySelector("input[name= 'quiz]:checked").value;   
-    if(userAnswer === correctAnswer){
+    if (userAnswer === correctAnswer){
         document.getElementById("feedback").textContent = "Correct! Well done."
     }  
 
